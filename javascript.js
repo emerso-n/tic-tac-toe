7b2ce387fbe2a664346e0ce626478e07515d0f99
Emerson <63008698+emerso-n@users.noreply.github.com>
2023-05-13 02:02:39 -0400

    init

---
+++ javascript.js
@@ -0,0 +1,12 @@
+console.log('JS LOADED')
+
+// so what does this code need to do
+// we need to be able to place the xs and os
+
+const gameboard = { 1: '' }
+
+const gridSpaces = document.querySelectorAll('.grid')
+
+function PlaceTokens () {
+
+}
