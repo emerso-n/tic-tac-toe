43411d8fdfa2884e0f4ec4f7b36a3db9d56c326d
Emerson <63008698+emerso-n@users.noreply.github.com>
2023-05-21 03:18:44 -0400

    it works

--- javascript-final.js
+++ javascript-final.js
@@ -103,95 +103,42 @@ const CPUPlayer = (() => {
         actualFreeTokens.push(grid.id)
       }
     })
-    const freeSpacesReal = [1, 2, 3, 4, 5]
-    // you have your list of empty spaces
-    // and your currentTokens
-    // you add an empty space to currentTokens
-    // you remove the space from the empty space array
-    // you recursion, with less empty spaces and more tokens in currenTokens
-    // let m = 0
-    function test (freeSpaces, thisPlayer) {
-      // if (GameManager.threeofsame.test(thisPlayer.currentTokens)) return 10 // i guess this just works
-      // if (freeSpaces <= 0) return 0
-      // for (let i = 0; i < freeSpaces.length; i++) {
-      //   player.two.currentTokens.push(freeSpaces[i])
-      //   m++
-      //   console.log(`before - i: ${i}, m: ${m}, currentTokens: ${thisPlayer.currentTokens}, freespaces: ${freeSpaces}`)
 
-      //   const newFreeSpaces = freeSpaces.filter((n, index) => index !== i)
-      //   test(newFreeSpaces, thisPlayer)
-      //   player.two.currentTokens.pop()
-      //   m++
-      //   console.log(`after - i: ${i}, m: ${m}, currentTokens: ${thisPlayer.currentTokens}, freespaces: ${freeSpaces}`)
-      // }
-      thisPlayer.currentTokens = ['1a', '1b', '3b', '2c']
-      const twoofsame = /(.).*\1/
-      const match = thisPlayer.currentTokens.join('').match(twoofsame)
-      if (match) {
-        const found = actualFreeTokens.find(token => token.includes(match[1]))
-        console.log(found)
-      }
-    }
-    // test(actualFreeTokens, player.two)
-
-    function minimax (freeTokens, currPlayer, depth = 0, lastTokens = []) {
+    function minimax (freeTokens, currPlayer, lastTokens = []) {
       if (lastTokens && GameManager.threeofsame.test(lastTokens.join(''))) {
-        return currPlayer.CPUBool ? { bestScore: -10, depth } : { bestScore: 10, depth }
-      } else if (freeTokens.length === 0) return { bestScore: 0, depth }
+        return currPlayer.CPUBool ? { bestScore: -10 } : { score: 10 }
+      } else if (freeTokens.length === 0) return { score: 0 }
 
-      // let bestScore = currPlayer.CPUBool ? -Infinity : Infinity
-      const bestMove = { bestScore: currPlayer.CPUBool ? -Infinity : Infinity, depth: Infinity }
       const moves = []
       for (let i = 0; i < freeTokens.length; i++) {
         currPlayer.currentTokens.push(freeTokens[i])
 
         const newFreeSpaces = freeTokens.filter((n, index) => index !== i)
-        const score = minimax(newFreeSpaces, currPlayer.CPUBool ? player.one : player.two, depth + 1, currPlayer.currentTokens)
-        // console.log(score)
-        // this gets the best score for the current player, min or max
-        // console.log(Object.assign(score, { depth }))
+        const score = minimax(newFreeSpaces, currPlayer.CPUBool ? player.one : player.two, currPlayer.currentTokens)
         moves.push(Object.assign(score, { move: freeTokens[i] }))
-
-        if ((currPlayer.CPUBool === true && score.bestScore >= bestMove.bestScore && score.depth < bestMove.depth) || (currPlayer.CPUBool === false && score.bestScore <= bestMove.bestScore && score.depth < bestMove.depth)) {
-          bestMove.bestScore = score.bestScore
-          bestMove.bestMove = freeTokens[i]
-          bestMove.depth = score.depth
-          // if ((currPlayer.CPUBool && score.bestScore === 10) || (!currPlayer.CPUBool && score.bestScore === -10)) {
-          //   currPlayer.currentTokens.pop()
-          //   break
-          // }
-          // can we just break here because it already found a win? i guess it could also be a tie so just check for that
-        }
 
         currPlayer.currentTokens.pop()
       }
-      // const minormax = currPlayer.CPUBool ? 'Max' : 'Min'
-      // console.log(Object.assign({ minormax }, bestMove))
-
-      return bestMove // ok no so this one will return the best move for which ever play it is, min or max. where tf does it go tho? thats what i need to know
-      // it goes to score = minimax, and at the very end it goes to what = minimax
-      // u get it
-    }
-    // player.one.currentTokens = ['1AD', '3B', '3CD']
-    // player.two.currentTokens = ['1B', '1C4', '2B4D']
-    // const what = minimax(['2A', '2C', '3A4'], player.two)
-    // console.log(what)
-
-    function GetBestMove (actualFreeTokens, _player) {
-      const moves = []
-      for (let i = 0; i < actualFreeTokens.length; i++) {
-        const newFreeSpaces = actualFreeTokens.filter((n, index) => index !== i)
-        const move = minimax(newFreeSpaces, _player)
-        console.log(move)
-        moves.push(move)
+      const bestMove = { bestScore: currPlayer.CPUBool ? -Infinity : Infinity }
+      if (currPlayer.CPUBool) {
+        for (let i = 0; i < moves.length; i++) {
+          if (moves[i].score > bestMove.bestScore) {
+            bestMove.bestScore = moves[i].score
+            bestMove.bestMove = moves[i].move
+          }
+        }
+      } else {
+        for (let i = 0; i < moves.length; i++) {
+          if (moves[i].score < bestMove.bestScore) {
+            bestMove.bestScore = moves[i].score
+            bestMove.bestMove = moves[i].move
+          }
+        }
       }
-      console.log(moves)
+      return bestMove
     }
-    GetBestMove(actualFreeTokens, player.two)
-    // get best id
+
     const bestid = minimax(actualFreeTokens, player.two)
-    // console.log('end')
-    // console.log(bestid)
     GameManager.gridSpaces.every(grid => {
       if (grid.id === bestid.bestMove) {
         grid.click()
